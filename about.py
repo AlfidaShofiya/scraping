784f3bc06425310784c38bd6c8412eca1fda177a
AlfidaShofiya <89784463+AlfidaShofiya@users.noreply.github.com>
2022-05-09 10:56:32 +0700

    Add files via upload

---
+++ about.py
@@ -0,0 +1,15 @@
+def greetings():
+	print(
+	r'''
+	Follow Me on :
+	Github   : https://github.com/dadandw
+	Medium   : https://howdysysinfo.medium.com
+    Linkedin : https://linkedin.com/dadan-dahman-wahidi
+
+	Welcome to detik.com Scraper!
+	Scrape all articles in total of pages!
+
+	Include:
+	|  TITLE   |   CATEGORY   |   TIMEPUBLISHED   |   LINK  |
+
+	''')
